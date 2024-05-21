2df9166e707b6ae3b2c0ce17125ae3d093a48b2e
DeveloperSajidAhmed <developersajidahmed@gmail.com>
2024-05-21 15:34:01 +0600

    add

---
+++ last/loop.js
@@ -0,0 +1,21 @@
+var num = 8;
+var count =0;
+// for ( count = 1; count <=10; count + 1)
+for ( count = 1; count <=10; count++)
+    // 1 * 8 = 
+{
+    // console.log(count + " * " + num + " = " + num*count);
+    document.write(count + " * " + num + " = " + num*count);
+}
+
+
+<p id="demo"></p>
+
+
+
+let i = 0;
+while (i < 10) {
+ 
+  i++;
+}
+console.log(i);
