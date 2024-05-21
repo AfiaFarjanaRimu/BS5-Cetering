31fd37380ff62601a97eeae7667e0ed7cbbbaaec
DeveloperSajidAhmed <developersajidahmed@gmail.com>
2024-05-21 15:53:57 +0600

    hh

--- last/loop.js
+++ last/loop.js
@@ -4,18 +4,22 @@ var count =0;
 for ( count = 1; count <=10; count++)
     // 1 * 8 = 
 {
-    // console.log(count + " * " + num + " = " + num*count);
-    document.write(count + " * " + num + " = " + num*count);
+    console.log(count + " * " + num + " = " + num*count);
+    // document.write(count + " * " + num + " = " + num*count);
 }
 
 
-<p id="demo"></p>
 
-
-
-let i = 0;
-while (i < 10) {
- 
-  i++;
+function display()
+{
+  var number=document.getElementById("num").value;
+  var count=0;
+  var text="";
+  for( count = 1; count <= 10; count = count + 1) {
+    text =text + count + " * " + number + " = " + number*count + "<br>";
+  }
+  document.getElementById("Output").innerHTML = text;
 }
-console.log(i);
+
+
+
